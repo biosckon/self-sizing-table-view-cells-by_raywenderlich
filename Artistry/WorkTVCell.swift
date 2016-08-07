ab85e91c752c231b2717fcfed8ad7399db73a760
biosckon <biosckon@hotmail.com>
2016-08-07 15:21:20 +0200

    first push

--- Artistry/WorkTVCell.swift
+++ Artistry/WorkTVCell.swift
@@ -13,5 +13,4 @@ class WorkTVCell: UITableViewCell {
   @IBOutlet weak var workImageView: UIImageView!
   @IBOutlet weak var workTitleLabel: UILabel!
   @IBOutlet weak var moreInfoTextView: UITextView!
-  
 }
